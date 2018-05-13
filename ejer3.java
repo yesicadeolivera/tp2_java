61ca369c661add3d975ab0d7c8b446b635820b7d
Yesica de olivera <yesika.99.lujan@gmail.com>
2018-05-13 13:50:49 -0300

    Ejer4.java modificado

--- ejer3.java
+++ ejer3.java
@@ -1,17 +1,31 @@
-package test;
+package ejer3;
 import java.util.Scanner;
 public class ejer3 {
 	public static void main(String[]ar) {
 		Scanner teclado= new Scanner(System.in);
 		int n=0;
 		int num;
+		int par=0;
+		int impar=0;
 		
-		for(n=0;n<=10;n++)
+		for(n=0; n<=10; n++)
 		{
 			System.out.print("Ingrese numero: ");
 			num=teclado.nextInt();
 			
-			System.out.print("num: " +num);
+			System.out.print(" num:" +num);
+			System.out.print("\n");
+		if(num%2==0)
+			{
+			par++;
+		    System.out.print("Es par" );	
+			}else{
+			impar++;
+			System.out.print("Es impar");
+			}
+		
 		}
+		System.out.print("Pares: "+par);
+		System.out.print("Impar: "+impar);
 	}
 }	
---
+++ ejer4.java
@@ -0,0 +1,19 @@
+package ejer4;
+import java.util.Scanner;
+public class ejer4 {
+public static void main(String[]ar) {
+	
+	int n=0;
+	int num=0;	
+	int resultado;
+	
+	for(n=0;n<=10;n++)
+	{
+		System.out.print(num+"*5=" );
+		resultado=num*5;
+		System.out.print(+resultado);
+		System.out.print("\n");
+		num++;
+	}	
+}
+}
